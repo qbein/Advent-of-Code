e0840dd25afe501530917652c4c4ae6ccab9171e
Kurt Aadnøy <kurt@aadnoy.info>
2021-12-27 23:15:04 +0100

    Working day 14 part 2

--- december_14.py
+++ december_14.py
@@ -1,41 +1,68 @@
 import helpers
+import re
+#import numpy as np
 
-def part01():  
-  template:str = None
-  rules:dict = None
+def part01():
+    result = calculate("data/14.txt", 10)
+    print("December 14, Part 1; result: {0}".format(result))
+    assert result == 2408
 
-  for line in helpers.yield_lines("data/14.txt"):
-    if(template is None):
-      template = line.strip()
-      continue
-    if(rules is None):
-      rules = {}
-      continue
-    key, insert = line.split(" -> ")
-    rules[key] = insert
-
-  input = template
-  for step in range(0, 10):
-    output = ""
-    l = len(input)
-    for i in range(0, l):
-      output += input[i]
-      if(i < l-1):
-        key = input[i] + input[i+1]
-        if key in rules:
-          output += rules[key]
-    input = output
-  
-  stats = {}
-  for c in output:
-    if c not in stats:
-      stats[c] = 0
-    stats[c] += 1
-
-  result = stats[max(stats, key=stats.get)] - stats[min(stats, key=stats.get)]
-  
-  print("December 14, Part 1; result: {0}".format(result))
-  assert result == 2408
 
 def part02():
-  print(2)
+    result = calculate("data/14.txt", 40)
+    print("December 14, Part 2; result: {0}".format(result))
+    assert result == 2408
+
+def calculate(filename, iter):
+    template: str = None
+    rules: dict = None
+
+    for line in helpers.yield_lines(filename):
+        if template is None:
+            template = line.strip()
+            continue
+        if rules is None:
+            rules = {}
+            continue
+        key, insert = line.split(" -> ")
+        rules[key] = key[0] + insert + key[1]
+    
+    print('rules', rules)
+    print('template', template)
+
+    #regex = re.compile("?=" + "|".join(map(re.escape, rules.keys())))
+    #print(regex)
+
+    output = list(template)
+
+    for step in range(0, iter):
+        matches = {}
+        l = len(output)
+        for rule in rules:
+            for i in range(0, l):
+                if(i > l-2):
+                    break
+                if(output[i] == rule[0] and output[i+1] == rule[1]):
+                    matches[i] = rules[rule]
+        for i in sorted(matches, reverse=True):
+            output[i:i+2] = list(matches[i])
+
+        #template = "".join(output)
+        #print(template)
+            
+        #matches = list(regex.finditer(template))
+        #matches.reverse()
+        #for m in matches:
+
+        #    print("start", m.start(), "group", m)
+        #template = regex.sub(lambda mo: rules[mo.group(0)], template)
+        #print(template)
+        print("Step: {}, len: {}".format(step, len(output)))
+
+    stats = {}
+    for c in output:
+        if c not in stats:
+            stats[c] = 0
+        stats[c] += 1
+
+    return stats[max(stats, key=stats.get)] - stats[min(stats, key=stats.get)]
